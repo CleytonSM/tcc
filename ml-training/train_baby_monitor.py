ca0c45be83cb2faea57c38ebda928b6a92ed5cff
CleytonSM <cleytonsm1309@gmail.com>
2026-03-06 00:43:47 -0300

    feat: adapt train_baby_monitor

--- ml-training/train_baby_monitor.py
+++ ml-training/train_baby_monitor.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
-import torch
-import torch_directml
 
 def main():
     # Load model
     model = YOLO("yolov8n.pt")
 
-    model.train(data="ml-training/baby_monitor.yaml", epochs=30)
+    model.train(
+        data="ml-training/baby_monitor.yaml",
+        epochs=100,
+        imgsz=640,
+        patience=50,
+        batch=16
+    )
     metrics = model.val()
 
 if __name__ == "__main__":
